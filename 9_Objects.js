0daf3857fe6b0d29cb52c922c174e685ff840dbd
Binod Raj Dhami <binodraazdhami@gmail.com>
2025-05-19 22:01:07 +0545

    object

---
+++ 9_Objects.js
@@ -0,0 +1,64 @@
+// Complex Data Type
+// ProtoType Base OOP
+// Key Pair maa value basxa
+
+// // Constructor Object
+// class Student {
+
+// }
+// let obj2 = new Student();
+
+// Literal Systax
+let student = {
+    name: "Ram",
+    email: "ram@gmail.com",
+    contact: {
+        tempAddress: "Kathamandu",
+        permAddress: 'Sunsari'
+    },
+    phone: [984300000, 9804000000],
+    isStudying: false
+}
+
+// console.log("student ", student);
+
+let car = {
+    wheel: 4,
+    color: "Black",
+    model: 2025,
+    price: 25000,
+    brand: "Toyota"
+}
+
+let mobile = {
+    brand: "Samsung",
+    storage: 64,
+}
+
+console.log("Before Adding : ", mobile);
+
+// Dot (.) Notation and Bracket Notation
+
+// Dot (.) Notation
+mobile.model = "Galaxy Ultra 24";
+mobile.storage = 128;
+
+// Bracket Notation
+mobile['price'] = 24000;
+
+let keyName = "screenType";
+mobile[keyName] = "LED";
+
+console.log("After Adding : ", mobile)
+
+// Create 10 Objects
+// ==============================
+// Car
+// mobile
+// student
+// room
+
+
+
+
+
